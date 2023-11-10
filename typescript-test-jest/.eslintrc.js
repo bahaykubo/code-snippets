f98e570d5f6c2a86a2d2c877038a9fd26338b575
noelsevilla <noel.sev@gmail.com>
2023-11-11 09:11:47 +1300

    Add example api tests in jest typescript

--- typescript-test-jest/.eslintrc.js
+++ typescript-test-jest/.eslintrc.js
@@ -1,3 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const rules = require('style-lint-js/.eslintrc-ts');
+
+rules.env = {
+  ...rules.env,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'jest/globals': true,
+};
+rules.plugins = ['eslint-plugin-jest'];
+rules.parserOptions.ecmaVersion = 'latest';
+rules.extends.push('plugin:jest/recommended');
+rules.rules = {
+  ...rules.rules,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'jest/consistent-test-it': [
+    'error',
+    {
+      fn: 'it',
+      withinDescribe: 'it',
+    },
+  ],
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'jest/expect-expect': 'off',
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'jest/no-conditional-expect': 'off',
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'jest/no-disabled-tests': 'off',
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'jest/no-standalone-expect': 'off',
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'jest/require-top-level-describe': 2,
+};
 
 module.exports = rules;
--- typescript-test-jest/jest.config.ts
+++ typescript-test-jest/jest.config.ts
@@ -4,9 +4,17 @@ export default (): Config.InitialOptions => {
   return {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/test/**/*.spec.ts'],
+    testMatch: ['**/tests/**/*.test.ts'],
     testTimeout: 30000,
     verbose: true,
     reporters: ['default'],
+    moduleNameMapper: {
+      /* eslint-disable @typescript-eslint/naming-convention */
+      '^@config/(.*)$': '<rootDir>/config/$1',
+      /* eslint-disable @typescript-eslint/naming-convention */
+      '^@constant/(.*)$': '<rootDir>/constants/$1',
+      /* eslint-disable @typescript-eslint/naming-convention */
+      '^@service/(.*)$': '<rootDir>/services/$1',
+    },
   };
 };

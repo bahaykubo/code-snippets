4cccc51da3ae4bd2040a56e995527484e1903ad2
noelsevilla <noel.sev@gmail.com>
2026-06-24 11:38:07 +1200

    Fix lint and errors in javascript-db-client project

--- javascript-db-client/eslint.config.mjs
+++ javascript-db-client/eslint.config.mjs
@@ -1,4 +1,12 @@
 import mocha from 'eslint-plugin-mocha';
 import styleLintJs from 'style-lint-js/eslint.config.js';
 
-export default [...styleLintJs, mocha.configs.recommended];
+export default [
+  ...styleLintJs,
+  mocha.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+    },
+  },
+];

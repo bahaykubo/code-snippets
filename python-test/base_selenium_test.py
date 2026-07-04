28dab713319abe03fd4cb5fdca6323dce7cdbd18
noelsevilla <noel.sev@gmail.com>
2026-07-05 09:32:29 +1200

    Fix python-test workflows

--- python-test/base_selenium_test.py
+++ python-test/base_selenium_test.py
@@ -25,7 +25,9 @@ class Browser:
         # self.browser.set_window_size(1280, 1024)
 
         options = Options()
-        # options.add_argument('--headless')
+        options.add_argument('--headless=new')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-extensions')
         options.add_argument('--disable-logging')
